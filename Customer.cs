b0dc47fa0a8f8401f8512f0be265b4be192ddb81
NguyenAn666 <97713637+NguyenAn666@users.noreply.github.com>
2022-08-22 10:15:29 +0700

    Add files via upload

---
+++ Customer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyCSharpApplication
+{
+    internal class Customer
+    {
+        private string name;
+        private int age;
+        private List<BankAccount> accounts;
+
+        public Customer(string name , int age, List<BankAccount> accounts)
+        {
+            this.name = name;
+            this.age = age;
+            this.accounts = new List<BankAccount>();
+        }
+    }
+}
